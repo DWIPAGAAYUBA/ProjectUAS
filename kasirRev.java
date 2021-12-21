0c720c98c3a323dc3a2db81294e069363fcf41d0
Frisaranda Diouf Julio <88955457+dioppp@users.noreply.github.com>
2021-12-21 19:48:00 +0700

    BISMILLAH FINAL

--- kasirRev.java
+++ kasirRev.java
@@ -1,4 +1,6 @@
 import java.util.Scanner;
+import java.util.Date;
+
 public class kasirRev {
     public static String metodeBayar(int pil) {
         String pilihanBayar = " ";
@@ -11,20 +13,65 @@ public class kasirRev {
         }
         return pilihanBayar;
     }
+    public static int jumlahBrg;
+    public static String namaBrg[];
+    public static int barang[][];
+    public static double hargaItem[];
+    public static int stok[] = {100/*AQUA*/, 50/*SPRITE*/, 50/*KOPIKAP*/, 30/*LAYS*/, 30/*RINGBEE*/,
+                                40/*OREO*/, 40/*BETTER*/, 100/*INDOMIE*/, 50/*MILO*/, 50/*BISKUAT*/};
+    public static void fiturStok(String namaBarang, int jumlahBarang){
+        switch (namaBarang.toLowerCase()) {
+            case "aqua":
+                stok[0] -= jumlahBarang;
+                break;
+            case "sprite":
+                stok[1] -= jumlahBarang;
+                break;
+            case "kopikap":
+                stok[2] -= jumlahBarang;
+                break;
+            case "lays":
+                stok[3] -= jumlahBarang;
+                break;
+            case "ringbee":
+                stok[4] -= jumlahBarang;
+                break;
+            case "oreo":
+                stok[5] -= jumlahBarang;
+                break;
+            case "better":
+                stok[6] -= jumlahBarang;
+                break;
+            case "indomie":
+                stok[7] -= jumlahBarang;
+                break;
+            case "milo":
+                stok[8] -= jumlahBarang;
+                break;
+            case "biskuat":
+                stok[9] -= jumlahBarang;
+                break;
+            default:
+                break;
+        }
+    }
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
+        Date tanggal = new Date();
         System.out.print("Masukkan nama kasir: "); 
         String namaKasir = sc.nextLine();
 
-        String namaBarang = "produk", gopay, dana, ovo;
-        int pilihan, jumlahBeli = 0, pilihBayar = 0, pin = 0, ewallet;
-        double hargaBarang = 0, totalBarang = 0, kembalian = 0, diskon = 0, stlhDiskon = 0, ppn = 0, totalFinal = 0, nominal = 0;
+        String nomor = "0";
+        int pilihan, pilihBayar = 0, pin = 0, ewallet;
+        double totalBarang = 0, kembalian = 0, diskon = 0, stlhDiskon = 0, ppn = 0, totalFinal = 0, nominal = 0;
         do {
             System.out.println();
             System.out.println("Pilih menu transaksi:");
-            System.out.println("1. Input Data Penjualan");
-            System.out.println("2. Pembayaran");
-            System.out.println("3. Cetak Nota Transaksi");
+            System.out.println("1. Stok Produk");
+            System.out.println("2. Input Data Penjualan");
+            System.out.println("3. Pembayaran");
+            System.out.println("4. Cetak Nota Transaksi");
+            System.out.println("5. Sisa Stok Produk");
             System.out.print("Pilihan menu: ");
             pilihan = sc.nextInt();
             sc.nextLine();
@@ -32,15 +79,44 @@ public class kasirRev {
             switch(pilihan){
                 case 1:
                     System.out.println();
-                    System.out.print("Masukkan nama barang: ");
-                    namaBarang = sc.nextLine();
-                    System.out.print("Masukkan harga barang: ");
-                    hargaBarang = sc.nextDouble();
+                    System.out.println("INFORMASI STOK PRODUK");
+                    System.out.println("AQUA        : " + stok[0]);
+                    System.out.println("SPRITE      : " + stok[1]);
+                    System.out.println("KOPIKAP     : " + stok[2]);
+                    System.out.println("LAYS        : " + stok[3]);
+                    System.out.println("RINGBEE     : " + stok[4]);
+                    System.out.println("OREO        : " + stok[5]);
+                    System.out.println("BETTER      : " + stok[6]);
+                    System.out.println("INDOMIE     : " + stok[7]);
+                    System.out.println("MILO        : " + stok[8]);
+                    System.out.println("BISKUAT     : " + stok[9]);
+                    break;
+                case 2:
+                    System.out.println();
+                    System.out.print("Masukkan jumlah produk: ");
+                    jumlahBrg = sc.nextInt();
                     sc.nextLine();
-                    System.out.print("Masukkan jumlah barang: ");
-                    jumlahBeli = sc.nextInt();
-                    sc.nextLine();
-                    totalBarang = hargaBarang * jumlahBeli;
+
+                    namaBrg = new String[jumlahBrg];
+                    barang = new int[jumlahBrg][jumlahBrg];
+                    hargaItem = new double[jumlahBrg];
+
+                    for(int i = 0; i < jumlahBrg; i++){
+                        System.out.println();
+                        System.out.println("Barang ke-" + (i+1));
+                        System.out.print("Masukkan nama barang: ");
+                        namaBrg[i] = sc.nextLine();
+                        System.out.print("Masukkan harga barang: ");
+                        barang[i][0] = sc.nextInt();
+                        sc.nextLine();
+                        System.out.print("Masukkan jumlah barang: ");
+                        barang[i][1] = sc.nextInt();
+                        sc.nextLine();
+
+                        fiturStok(namaBrg[i], barang[i][1]);
+                        hargaItem[i] = (double)barang[i][0] * barang[i][1];
+                        totalBarang += hargaItem[i];
+                    }
 
                     if(totalBarang >= 20000){
                         diskon = totalBarang * 0.2;
@@ -53,7 +129,7 @@ public class kasirRev {
                     ppn = totalBarang * 0.1;
                     totalFinal = stlhDiskon + ppn;
                     break;
-                case 2:
+                case 3:
                     System.out.println();
                     System.out.println("Pilih metode pembayaran:");
                     System.out.println("1. Cash");
@@ -73,7 +149,6 @@ public class kasirRev {
                             pin = sc.nextInt();
                             System.out.print("Masukkan nominal: ");
                             nominal = sc.nextDouble();
-                            kembalian = nominal - totalFinal;
                             break;
                         case 3:
                             System.out.println("Pilih metode pembayaran e-Wallet:");
@@ -86,21 +161,21 @@ public class kasirRev {
                             switch(ewallet){
                                 case 1:
                                     System.out.print("Masukkan nomor Gopay: ");
-                                    gopay = sc.nextLine();
+                                    nomor = sc.nextLine();
                                     sc.nextLine();
                                     System.out.print("Masukkan nominal: ");
                                     nominal = sc.nextDouble();
                                     break;
                                 case 2:
                                     System.out.print("Masukkan nomor DANA: ");
-                                    dana = sc.nextLine();
+                                    nomor = sc.nextLine();
                                     sc.nextLine();
                                     System.out.print("Masukkan nominal: ");
                                     nominal = sc.nextDouble();
                                     break;
                                 case 3:
                                     System.out.print("Masukkan nomor OVO: ");
-                                    ovo = sc.nextLine();
+                                    nomor = sc.nextLine();
                                     sc.nextLine();
                                     System.out.print("Masukkan nominal: ");
                                     nominal = sc.nextDouble();
@@ -111,32 +186,48 @@ public class kasirRev {
                             System.exit(0);
                     }
                     break;
-                case 3:
+                case 4:
                     System.out.println();
-                    System.out.println("DATA PENJUALAN BARANG");
-                    System.out.println("PT ABADI SEJAHTERA");
+                    System.out.println("ABADI SEJAHTERA MART");
+                    System.out.println("Waktu Transaksi: " + tanggal);
                     System.out.println("Nama Kasir: " + namaKasir);
-                    System.out.println("====================================================================");
+                    System.out.println("--------------------------------------------------------------------");
                     System.out.println("No. \tNama Barang \tHarga Satuan \tJumlah \t\tHarga Total");
-                    System.out.println("====================================================================");
-                    System.out.println("1. \t" + namaBarang + "\t\t" + hargaBarang + "\t\t" + jumlahBeli + "\t\t" + totalBarang);
-                    System.out.println("====================================================================");
-                    System.out.println("====================================================================");
-                    System.out.println("HARGA JUAL \t\t\t: Rp" + totalBarang);
-                    System.out.println("DISKON \t\t\t\t: Rp" + diskon);
-                    System.out.println("PPN \t\t\t\t: Rp" + ppn);
-                    System.out.println("Cara Bayar (Cash/Debit/e-Wallet): " + metodeBayar(pilihBayar));
-                    System.out.println("TOTAL \t\t\t\t: Rp" + totalFinal);
-                    System.out.println("====================================================================");
-                    System.out.println("NOMINAL UANG ANDA \t\t: " + nominal);
+                    System.out.println("--------------------------------------------------------------------");
+                    for(int x = 0; x < jumlahBrg; x++){
+                        System.out.println((x+1) + "\t" + namaBrg[x] + "\t\t" + barang[x][0] + "\t\t" + barang[x][1] + "\t\t" + hargaItem[x]);
+                    }
+                    System.out.println("--------------------------------------------------------------------");
+                    System.out.println("--------------------------------------------------------------------");
+                    System.out.println("                                    HARGA JUAL      : Rp " + totalBarang);
+                    System.out.println("                                    DISKON          : Rp " + diskon);
+                    System.out.println("                                    PPN             : Rp " + ppn);
+                    System.out.println("                                    Cara Bayar      : " + metodeBayar(pilihBayar));
+                    System.out.println("                                    TOTAL           : Rp " + totalFinal);
+                    System.out.println("                                   ---------------------------------");
+                    System.out.println("                                    TUNAI           : Rp " + nominal);
                     if(pilihBayar == 1){
-                        System.out.println("KEMBALIAN \t\t\t: " + kembalian);
+                        System.out.println("                                    KEMBALIAN       : Rp " + kembalian);
                     } else if (pilihBayar == 2){
                         System.out.println();
                     } else if (pilihBayar == 3){
                         System.out.println();
                     }
+                    break;
+                case 5:
+                    System.out.println();
+                    System.out.println("INFORMASI STOK PRODUK");
+                    System.out.println("AQUA        : " + stok[0]);
+                    System.out.println("SPRITE      : " + stok[1]);
+                    System.out.println("KOPIKAP     : " + stok[2]);
+                    System.out.println("LAYS        : " + stok[3]);
+                    System.out.println("RINGBEE     : " + stok[4]);
+                    System.out.println("OREO        : " + stok[5]);
+                    System.out.println("BETTER      : " + stok[6]);
+                    System.out.println("INDOMIE     : " + stok[7]);
+                    System.out.println("MILO        : " + stok[8]);
+                    System.out.println("BISKUAT     : " + stok[9]);
             }
-        } while (pilihan !=3);
+        } while (pilihan !=5);
     }
 }
